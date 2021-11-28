a296566032cb2347da4872abb7b791dae5544d5f
Victor Vermeulen <vcvermeulen@gmail.com>
2021-11-28 19:19:25 +0100

    first commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SeasonDisplay = (props) => {
+	return <div>Season Display</div>;
+};
+
+SeasonDisplay.propTypes = {};
+
+export default SeasonDisplay;
