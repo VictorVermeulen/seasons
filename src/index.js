2c512b643a5711115bada654afb5ce15b0a3ef9d
Victor Vermeulen <vcvermeulen@gmail.com>
2021-11-30 20:37:34 +0100

    lifecyle methods

--- src/index.js
+++ src/index.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export class App extends Component {
-	render() {
-		navigator.geolocation.getCurrentPosition(
-			(position) => console.log(position),
-			(err) => console.log(err)
-		);
+	constructor(props) {
+		super(props);
+		this.state = { lat: null, errorMessage: '' };
+	}
 
-		return <div>Latitude:</div>;
+	componentDidMount() {
+		navigator.geolocation.getCurrentPosition(
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
+		);
+		console.log('My component was rendered to the screen');
+	}
+
+	componentDidUpdate() {
+		console.log('My component was just updated to the screen');
+	}
+
+	render() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error:{this.state.errorMessage}</div>;
+		} else if (this.state.lat) {
+			return <div>Latitude: {this.state.lat}</div>;
+		} else {
+			return <div>Loading</div>;
+		}
 	}
 }
 
