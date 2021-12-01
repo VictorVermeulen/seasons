aff144dd4e160c4b098b14cec7d83022b0acd7ee
Victor Vermeulen <vcvermeulen@gmail.com>
2021-12-01 19:09:04 +0100

    state instead of constructor

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SeasonDisplay = (props) => {
+	console.log(props);
+
 	return <div>Season Display</div>;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 export class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { lat: null, errorMessage: '' };
-	}
+	state = { lat: null, errorMessage: '' };
 
 	componentDidMount() {
 		navigator.geolocation.getCurrentPosition(
@@ -23,7 +21,7 @@ export class App extends Component {
 		if (this.state.errorMessage && !this.state.lat) {
 			return <div>Error:{this.state.errorMessage}</div>;
 		} else if (this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		} else {
 			return <div>Loading</div>;
 		}
