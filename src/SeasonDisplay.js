e8b019d6bfbe9d386a1c203b54bff1f4c362c11c
Victor Vermeulen <vcvermeulen@gmail.com>
2021-12-02 11:27:13 +0100

    add icon and text

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SeasonDisplay = (props) => {
-	console.log(props);
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) {
+		return lat > 0 ? 'summer' : 'winter';
+	} else {
+		return lat > 0 ? 'winter' : 'summer';
+	}
+};
 
-	return <div>Season Display</div>;
+const SeasonDisplay = (props) => {
+	const season = getSeason(props.lat, new Date().getMonth);
+
+	const text =
+		season === 'winter' ? 'Brrrr, Ice Cold!' : 'Its getting hot in here!';
+	const icon = season === 'winter' ? 'grey snowflake' : 'yellow sun';
+
+	return (
+		<div>
+			<i className={`massive ${icon} icon`}></i>
+			<h1>{text}</h1>
+			<i className={`massive ${icon} icon`}></i>
+		</div>
+	);
 };
 
 SeasonDisplay.propTypes = {};
